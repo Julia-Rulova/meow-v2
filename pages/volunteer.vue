371e0305fd9a7d365a7388c099af1f3b8c682ff8
Julia-Rulova <julia.rulova0066@gmail.com>
2024-03-28 18:10:11 +0300

    адаптивность о волонтере

--- pages/volunteer.vue
+++ pages/volunteer.vue
@@ -2,33 +2,40 @@
   <section
     class="flex flex-col gap-6 justify-between w-full flex-1 min-h-screen"
   >
-    <div class="flex flex-col gap-6">
+    <div class="flex flex-col gap-3 lg:gap-6">
       <LayoutHeader></LayoutHeader>
 
       <UHorizontalNavigation
         :links="links"
-        class="border-b border-gray-200 dark:border-gray-800 px-8"
+        class="border-b px-8 bg-transparent static border-t-0"
         :ui="{
-          base: 'text-base',
+          base: 'justify-center sm:justify-start md:text-base',
+          label: 'hidden sm:inline',
+          inner: 'flex-1 sm:flex-auto',
+          container: 'w-full sm:w-auto',
         }"
       />
 
-      <div class="flex flex-row items-start mx-36 gap-16 mt-8">
-        <div class="flex flex-col h-full">
-          <div class="relative min-w-72 mb-12">
+      <div
+        class="flex flex-col items-start mx-4 gap-4 sm:mx-10 sm:gap-8 md:mx-36 lg:gap-16 lg:flex-row mt-3 sm:mt-8"
+      >
+        <div
+          class="flex h-full flex-col items-center justify-center w-full sm:flex-row sm:justify-start sm:items-end lg:items-start lg:flex-col"
+        >
+          <div class="relative lg:min-w-none sm:min-w-72 lg:mb-12">
             <div
-              class="border-4 border-primary w-72 h-72 absolute -bottom-8 -left-8 z-0 rounded-xl"
+              class="hidden sm:flex border-4 border-primary w-52 h-52 rounded-xl relative z-0 lg:absolute lg:-bottom-8 lg:-left-8 lg:w-72 lg:h-72"
             ></div>
 
             <img
               :src="user.ava"
               :alt="user.name"
-              class="object-cover w-72 h-72 z-10 relative rounded-xl"
+              class="object-cover h-52 w-full z-10 shadow-[6px_3px_25px_-7px_rgba(0,0,0,0.72)] rounded-xl sm:w-52 sm:absolute sm:-top-4 sm:left-4 lg:relative lg:top-0 lg:left-0 lg:w-72 lg:h-72"
             />
           </div>
 
           <ul
-            class="flex flex-col gap-3 -ml-8 border-l-2 pl-2 border-primary border-dashed"
+            class="hidden sm:flex flex-row flex-wrap gap-3 border-primary border-dashed flex-col -ml-8 border-l-2 pl-2 h-fit lg:h-auto"
           >
             <li>
               <a
@@ -54,7 +61,7 @@
           </ul>
         </div>
 
-        <div class="flex flex-col gap-7">
+        <div class="flex flex-col gap-3 sm:gap-7">
           <h3
             class="text-6xl uppercase [font-family:monospace] text text-primary font-semibold"
           >
@@ -63,6 +70,32 @@
 
           <p>{{ user.descr }}</p>
         </div>
+
+        <ul
+          class="flex flex-row flex-wrap gap-3 mt-4 border-primary border-t-2 pt-1.5 border-dashed sm:hidden"
+        >
+          <li>
+            <a
+              :href="'tel:' + user.phone"
+              class="flex flex-row items-center gap-2"
+            >
+              <Icon name="pepicons-pop:phone-circle" class="w-5 h-5"></Icon>
+              <span> {{ user.phone }}</span>
+            </a>
+          </li>
+          <li>
+            <a :href="user.telegram" class="flex flex-row items-center gap-2">
+              <Icon name="streamline:telegram" class="w-5 h-5"></Icon>
+              <span> {{ user.telegram }}</span>
+            </a>
+          </li>
+          <li>
+            <a :href="user.inst" class="flex flex-row items-center gap-2">
+              <Icon name="fa6-brands:instagram" class="w-5 h-5"></Icon>
+              <span>{{ user.inst }}</span>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
 
